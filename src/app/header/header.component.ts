e318941b6c54ae1b154de10c92f5bfaee8ec506b
aranduan <aranduandikoetxea@gmail.com>
2025-01-25 19:12:15 +0100

    Header_Data_Binding_Directivas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  title = 'Cocina con Mónica';
+export class HeaderComponent implements OnInit {
+  appName = 'Cocina con Mónica';
+  menuItems = [
+    { label: 'Bienvenida', route: '/welcome' },
+    { label: 'Login', route: '/login' },
+    { label: 'Home', route: '/home' },
+    { label: 'Recetas', route: '/recipe-list' },
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }

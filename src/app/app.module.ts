820f85b2392bcee3a9f0ff99dcb57097f2ff66d8
MarinaVargas19 <m.vcampos.2019@gmail.com>
2025-01-25 17:26:37 +0100

    styles_bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
 
  @NgModule({
     declarations: [
      AppComponent,
-     WelcomeComponent
+     WelcomeComponent,
+     LoginComponent
     ],
      imports: [
      BrowserModule,
